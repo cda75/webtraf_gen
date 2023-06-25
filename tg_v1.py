b6aa2be6300eacda4859b7001092dc40543dd85d
cda75 <dchestnov@mail.ru>
2023-06-25 15:55:39 +0300

    OS support

--- tg_v1.py
+++ tg_v1.py
@@ -9,10 +9,17 @@ from random import randint
 from bs4 import BeautifulSoup as bs4
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from time import sleep
+from sys import platform
+from os import path
+from platform import system
 
 #URL list
 url_file = "urls.txt"
+workDir = path.dirname(path.abspath(__file__))
+drv_exec = '/chromedriiver' if system() == 'Linux' else '/chromedriver.exe' 
+workDir = workDir+drv_exec
 
 def get_urls(u_file):
     with open(u_file, 'r') as f:
@@ -24,14 +31,14 @@ def get_links_from_page(url, n=1, with_download=False):
                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) Chrome/90.0.4430.85 Safari/537.36'}
     r = requests.get(url, headers=headers)
     html = r.text
-    #print(url, r.status_code, len(html))
     if (r.status_code == 200) and (len(html)) > 0:
         bs = bs4(html, features='html.parser')
         hrefs = [href.get("href") for href in bs.find_all('a')]   
         rand_urls = []
         count = 0
         while count < n:
-            rand_href = hrefs[randint(0,len(hrefs))]
+            rand_num = randint(0,len(hrefs))
+            rand_href = hrefs[rand_num]
             if 'https' in rand_href:
                 rand_urls.append(rand_href)
                 count += 1
@@ -43,12 +50,13 @@ def get_links_from_page(url, n=1, with_download=False):
 
 def browser(url):
     options = Options()
+    service = Service(executable_path = './chromedriver')
     options.add_argument('--headless=new')
-    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver = webdriver.Chrome(service=service, options=options)
     try:
         driver.get(url)
         print(driver.current_url, driver.title)
-        sleep(2)
+        #sleep(2)
     except BaseException as error:
         print(f'Error when accessng {url}')
         print(error)
@@ -62,8 +70,8 @@ def main():
     urls = get_urls(url_file)
     for url in urls:
         rand_urls = get_links_from_page(url,2)
-        for rand_url in rand_urls:
-            browser(rand_url)
+        for url in rand_urls:
+            browser(url)
         
 
         
